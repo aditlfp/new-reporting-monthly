198518442758946cc6afba151e576b4c95321f6f
syafi-M <syafimq00@gmail.com>
2025-12-10 13:56:24 +0700

    fix: update accepted image types for upload functionality

--- resources/views/pages/user/send_img/create.blade.php
+++ resources/views/pages/user/send_img/create.blade.php
@@ -77,8 +77,7 @@
 						                            ]
 						                        ];
 						                        
-						                        // $acceptedTypes = '.gif,.tif,.tiff,.png,.crw,.cr2,.dng,.raf,.nef,.nrw,.orf,.rw2,.pef,.arw,.sr2,.raw,.psd,.svg,.webp,.heic,.jpg,.jpeg';
-                                                $acceptedTypes = 'image/*,.jpg,.jpeg,.png,.webp,.heic';
+						                        $acceptedTypes = '.gif,.tif,.tiff,.png,.crw,.cr2,.dng,.raf,.nef,.nrw,.orf,.rw2,.pef,.arw,.sr2,.raw,.psd,.svg,.webp,.heic,.jpg,.jpeg';
 						                        
 						                        $uploadIcon = '<svg class="w-5 h-5 mb-1 sm:w-6 sm:h-6 text-slate-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 						                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
